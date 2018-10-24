bf25888f6e32db919e51f5d69b6ed1b7b6d068e3
ThomasBuchinger <ThomasBuchinger@users.noreply.github.com>
2018-10-24 04:25:57 +0200

    Update serviceprovision_complete.rb

--- automate/buc/Service/Provisioning/Email.class/__methods__/serviceprovision_complete.rb
+++ automate/buc/Service/Provisioning/Email.class/__methods__/serviceprovision_complete.rb
@@ -1,4 +1,5 @@
 #
 # Description: Place holder for Service Provision Complete email #
+# A change
 #
 $evm.log("warn", "[DEPRECATION] This method will be deprecated. Please use similarly named method from System/Notification/Email class.")
